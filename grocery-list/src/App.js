8b45205d5bb09acd38771df1b5d2a6a1afe1015b
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-05 14:38:17 -0400

    added the ability to add a new item to the list

--- grocery-list/src/App.js
+++ grocery-list/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './Form';
 import Filters from './Filters';
 import List from './List';
@@ -12,19 +12,26 @@ const App = () => {
     { name: 'Bakery', value: 'bakery' },
   ];
 
-  const items = [
+  const initialItems = [
     { name: 'Steak', type: 'meat', quantity: 3 },
     { name: 'Apples', type: 'prod', quantity: 4 },
     { name: 'Milk (1L, 2%)', type: 'dairy', quantity: 1 },
     { name: 'Baguettes', type: 'bakery', quantity: 2 },
   ];
 
+  const [items, setItems] = useState(initialItems);
+
+  const addItem = (item) => {
+    const newItem = {name: item, type: 'all', quantity: 1}
+    setItems((currentItems) => [...currentItems, newItem]);
+  }
+
   return (
     <main className="layout" id="app">
       <header className="header">
         <h1>Grocery List</h1>
       </header>
-      <Form />
+      <Form onSubmit={addItem} />
       <Filters filters={ filters }/>
       <List items={ items } />
     </main>
--- grocery-list/src/Form.js
+++ grocery-list/src/Form.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-const Form = () => {
+const Form = ({ onSubmit }) => {
+
+  const itemRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Handled Submit`);
+    const newItem = itemRef.current.value;
+    if (newItem !== '') {
+      onSubmit(newItem);
+    }
+    itemRef.current.value = '';
+  }
+
   return (
-   <form id="newItem" className="newitem" auto-complete="off">
+   <form id="newItem" className="newitem" auto-complete="off" onSubmit={handleSubmit}>
      <label htmlFor="itemName" className="line-label">New Item</label>
      <div className="addnew">
-       <input type="text" name="item" id="itemName" className="form-component inpt" placeholder="What do you need?" />
+       <input type="text" ref={itemRef} name="item" id="itemName" className="form-component inpt" placeholder="What do you need?" />
        <input type="submit" value="Add" className="form-component btn" />
      </div>
    </form>
